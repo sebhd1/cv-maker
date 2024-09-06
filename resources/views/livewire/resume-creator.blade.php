02e04127bfd2546d1a9f949a441fa672d4236e95
Sebastiano Ballotta <sebeballotta@outlook.com>
2024-09-06 15:04:51 +0200

    Adjusted course editor and formatted view

--- resources/views/livewire/resume-creator.blade.php
+++ resources/views/livewire/resume-creator.blade.php
@@ -1,5 +1,7 @@
 <div>
     <livewire:profile-editor />
     <livewire:employment-editor />
+    <livewire:education-editor />
     <livewire:course-editor />
+    <livewire:social-link-editor/>
 </div>
